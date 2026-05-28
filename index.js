a2f40b867ebcb59c52db4d732a18b2c8433d84c3
jose fabio <fabiosantos33e@gmail.com>
2026-05-27 23:38:53 -0300

    add express port

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Mostrinho online 😎🔥");
+});
+
+app.listen(3000, () => {
+  console.log("🌐 Porta aberta");
+});
+
 const {
   Client,
   GatewayIntentBits,
