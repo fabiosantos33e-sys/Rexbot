8870ccbc12f0c3a2447aec9040163bd83a69c42c
jose fabio <fabiosantos33e@gmail.com>
2026-05-27 22:41:31 -0300

    fix rest token

--- boasvindas.js
+++ boasvindas.js
@@ -1,47 +1,29 @@
-const {
-  EmbedBuilder
-} = require("discord.js");
+onst { EmbedBuilder } = require("discord.js");
 
 module.exports = (client) => {
 
-  client.on("guildMemberAdd", async member => {
+client.on("guildMemberAdd", async (member) => {
 
-    const canal =
-      member.guild.channels.cache.find(
-        c => c.name === "･ﾟ🦕-┊🌿┊💬◇『ℂ𝕙𝕒𝕥』◇💬┊🌿┊🦕･ﾟ"
-      );
+const canal = member.guild.channels.cache.find(
+c => c.name === "geral"
+);
 
-    if (!canal) return;
+if (!canal) return;
 
-    const embed =
-      new EmbedBuilder()
-        .setColor("#2b2d31")
-        .setTitle("🌙 Tum tum... Mostrinho apareceu!")
-        .setDescription(
-          `👋 Olá ${member}!\n\n` +
-          `✨ Seja muito bem-vindo(a) à nossa família.\n\n` +
-          `💜 Esperamos que você faça novas amizades,\n` +
-          `se divirta bastante\n` +
-          `e aproveite cada momento aqui conosco.\n\n` +
-          `🌑 O Mostrinho já está feliz com sua chegada!\n\n` +
-          `🎉 @here vem dar boas-vindas pro nosso novo membro!`
-        )
-        .setThumbnail(
-          member.user.displayAvatarURL({
-            dynamic: true
-          })
-        )
-        .setFooter({
-          text:
-            `Agora somos ${member.guild.memberCount} membros 💜`
-        })
-        .setTimestamp();
+const embed = new EmbedBuilder()
+.setColor("#2b2d31")
+.setTitle("🌙 Tum tum... Mostrinho apareceu!")
+.setDescription(
+`💜 Olá ${member}!\n\nSeja muito bem-vindo(a) ao servidor!\nEsperamos que você se divirta bastante com a gente 😎🔥\n\n🎉 @here vem dar boas-vindas pro novo membro!`
+)
+.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+.setTimestamp();
 
-    canal.send({
-      content: `💫 ${member}`,
-      embeds: [embed]
-    });
+canal.send({
+content: `${member}`,
+embeds: [embed]
+});
 
-  });
+});
 
 };
