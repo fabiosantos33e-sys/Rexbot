5c58413a221b8667893faaec6d0b0eb8e7b8d82c
jose fabio <fabiosantos33e@gmail.com>
2026-06-21 13:19:48 -0300

    Adicionado cargo de suporte nos tickets

--- ticket.js
+++ ticket.js
@@ -31,7 +31,7 @@ Escolha uma opção abaixo para abrir um ticket!
 🤝 Parcerias
 ⚙️ Funções
       `)
-      .setImage("https://cdn.discordapp.com/attachments/1495896642904129676/1508270269712568330/bb3f7ace-1ee0-40d6-bac4-3ad5c5e8a9ba.png?ex=6a14eda6&is=6a139c26&hm=d5f9095c411732a8c81cc54645f53d818e8ff7020d9ac67fa7eefd2b196473c7&.png");
+      .setImage("https://cdn.discordapp.com/attachments/1513675524013166753/1518283925666136218/075c2e1b-9769-41a5-954c-fdfc69f2ded6.png?ex=6a395b98&is=6a380a18&hm=083ac874bb0dc61e72e1b88130805f5337d286e9ecc3c9fcb250062b7c77aea2&.png");
 
       const row = new ActionRowBuilder()
       .addComponents(
@@ -92,20 +92,23 @@ Escolha uma opção abaixo para abrir um ticket!
 
         permissionOverwrites: [
 
-          {
-            id: interaction.guild.id,
-            deny: [PermissionFlagsBits.ViewChannel]
-          },
+        {
+  id: interaction.user.id,
+  allow: [
+    PermissionFlagsBits.ViewChannel,
+    PermissionFlagsBits.SendMessages,
+    PermissionFlagsBits.ReadMessageHistory
+  ]
+},
 
-          {
-            id: interaction.user.id,
-            allow: [
-              PermissionFlagsBits.ViewChannel,
-              PermissionFlagsBits.SendMessages,
-              PermissionFlagsBits.ReadMessageHistory
-            ]
-          }
-
+{
+  id: "1485441258204823603", // ID do cargo suporte
+  allow: [
+    PermissionFlagsBits.ViewChannel,
+    PermissionFlagsBits.SendMessages,
+    PermissionFlagsBits.ReadMessageHistory
+  ]
+}
         ]
 
       });
@@ -120,7 +123,7 @@ Seu ticket foi aberto com sucesso 🦖
 
 Explique seu problema e aguarde a equipe responder.
       `)
-      .setImage("https://cdn.discordapp.com/attachments/1495896642904129676/1508277079433806104/26f18974-5305-4e62-aecb-0ebd816a2856.png?ex=6a14f3fe&is=6a13a27e&hm=7690714d3ceea1c1f4e28a7601c704cdafed8273c6ff6196e20527b6850098a4&.png");
+      .setImage("https://cdn.discordapp.com/attachments/1513675524013166753/1518285973375680693/e9ea2be9-ab6e-496e-a9d4-a29f687522e8.png?ex=6a395d80&is=6a380c00&hm=42de35a0a0f916694e21cf6264568d53e7d1ebac37e6ebf59c08c306df53c683&. png");
       const fechar = new ActionRowBuilder()
       .addComponents(
 
@@ -132,11 +135,13 @@ Explique seu problema e aguarde a equipe responder.
 
       );
 
-      await canal.send({
-        content: `${interaction.user}`,
-        embeds: [embed],
-        components: [fechar]
-      });
+      const cargoSuporte = "1485441258204823603"; // ID do cargo
+
+await canal.send({
+  content: `${interaction.user} <@&${cargoSuporte}>`,
+  embeds: [embed],
+  components: [fechar]
+});
 
       return interaction.editReply({
         content: `✅ Ticket criado: ${canal}`
