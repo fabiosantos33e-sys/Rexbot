b183123b4a8dc3c0970bc83fea0d75e91c7c7129
jose fabio <fabiosantos33e@gmail.com>
2026-06-22 04:26:13 -0300

    add voice intents

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
+    GatewayIntentBits.GuildVoiceStates 
   ]
 });
 
